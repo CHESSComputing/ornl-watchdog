0c7b7af5fdc18a74de9653480ea7dea52a392d68
Keara Soloway <kls286@cornell.edu>
2026-04-24 10:53:40 -0400

    add: (most of) actual update pipeline configurations

--- app/config_writer.py
+++ app/config_writer.py
@@ -118,13 +118,13 @@ def create_dataset_configs(dataset_name, spec_file, scan_number):
             "setup_map": [
                 {
                     "common.YAMLReader": {
-                        "filename": map_yaml,
+                        "filename": str(map_yaml),
                         "schema": "common.models.map.MapConfig",
                     }
                 },
                 {
                     "common.YAMLReader": {
-                        "filename": _state.detector_yaml,
+                        "filename": str(_state.detectors_yaml),
                         "schema": "common.models.map.DetectorConfig",
                     }
                 },
@@ -137,7 +137,7 @@ def create_dataset_configs(dataset_name, spec_file, scan_number):
                 },
                 {
                     "common.NexusWriter": {
-                        "filename": data_nxs,
+                        "filename": str(data_nxs),
                         "force_overwrite": True,
                     }
                 },
@@ -145,19 +145,19 @@ def create_dataset_configs(dataset_name, spec_file, scan_number):
             "setup_strain": [
                 {
                     "common.YAMLReader": {
-                        "filename": _state.strain_analysis_yaml,
+                        "filename": str(_state.strain_analysis_yaml),
                         "schema": "edd.models.StrainAnalysisConfig",
                     }
                 },
                 {
                     "common.YAMLReader": {
-                        "filename": _state.calibration_yaml,
+                        "filename": str(_state.calibration_yaml),
                         "schema": "edd.models.MCATthCalibrationConfig",
                     }
                 },
                 {
                     "common.NexusReader": {
-                        "filename": data_nxs,
+                        "filename": str(data_nxs),
                     }
                 },
                 {
@@ -172,7 +172,7 @@ def create_dataset_configs(dataset_name, spec_file, scan_number):
                 },
                 {
                     "common.NexusWriter": {
-                        "filename": data_nxs,
+                        "filename": str(data_nxs),
                         "nxpath": f"/{dataset_name}_strain_analysis",
                         "force_overwrite": True,
                     }
@@ -198,9 +198,11 @@ def update_dataset_configs(dataset_name, scan_numbers):
     :param scan_numbers: SPEC scan numbers collected during this update.
     :type scan_numbers: list[int]
     """
-    analysis_dir = Path(get_state().analysis_root) / dataset_name
+    _state = get_state()
+    analysis_dir = Path(_state.analysis_root) / dataset_name
     map_yaml = analysis_dir / "map_config.yaml"
     pipeline_yaml = analysis_dir / "pipeline.yaml"
+    data_nxs = analysis_dir / f"{dataset_name}.nxs"
 
     # Update the map config with new scan numbers
     logger.debug(f"Updating {map_yaml}")
@@ -209,17 +211,82 @@ def update_dataset_configs(dataset_name, scan_numbers):
     map_config["spec_scans"][0]["scan_numbers"].extend(scan_numbers)
     with open(map_yaml, "w") as f:
         yaml.dump(map_config, f, sort_keys=False)
-      
+    logger.info(f"Updated {map_yaml}")
+
     # Update the pipeline config to include a new "update" step for the new scans
     logger.debug(f"Updating {pipeline_yaml}")
     with open(pipeline_yaml, "r") as f:
         pipeline_config = yaml.safe_load(f)
-    pipeline_config[
-        f"update_{get_state().datasets[dataset_name]['current_update']}"] = [
-        # FILL IN THIS LIST
+    update_i = _state.datasets[dataset_name]["current_update"]
+    logger.debug(f"update_i = {update_i}")
+    logger.debug(f"spec_file = {map_config['spec_scans'][0]['spec_file']}")
+    pipeline_config[f"update_map_{update_i}"] = [
+        {
+            "common.YAMLReader": {
+                "filename": str(map_yaml),
+                "schema": "common.models.map.MapConfig",
+            }
+        },
+        {
+            "common.MapSliceProcessor": {
+                "spec_file": map_config["spec_scans"][0]["spec_file"],
+                "scan_number": scan_numbers, # FIXME
+                "detectors": [{"id": 0}], # FIXME
+            }
+        },
+        {
+            "common.NexusValuesWriter": {
+                "filename": str(data_nxs),
+                "force_overwrite": True,
+                "resize_axis": 0,
+            }
+        },
     ]
+    logger.debug(f"Added pipeline: update_map_{update_i}")
+    pipeline_config[f"update_strain_{update_i}"] = [
+        {
+            "edd.SliceNXdataReader": {
+                "filename": str(data_nxs),
+                "scan_number": scan_numbers, # FIXME
+            }
+        },
+        {
+            "common.YAMLReader": {
+                "filename": str(_state.strain_analysis_yaml),
+                "schema": "edd.models.StrainAnalysisConfig",
+            }
+        },
+        {
+            "common.YAMLReader": {
+                "filename": str(_state.calibration_yaml),
+                "schema": "edd.models.MCATthCalibrationConfig",
+            }
+        },
+        {
+            "edd.StrainAnalysisProcessor": {
+                "standalone": True,
+                "setup": False,
+                "update": True,
+                "find_peaks": True,
+                "skip_animation": True,
+                "save_figures": False,
+            }
+        },
+        {
+            "common.NexusValuesWriter": {
+                "filename": str(data_nxs),
+                "path_prefix": f"/{dataset_name}_strain_analysis/",
+                "idx_slice": {
+                    "start": 0, # FIXME
+                    "stop": 1,
+                },
+                "resize_axis": 0,
+                "force_overwrite": True,
+            }
+        },
+    ]
+    logger.debug(f"Added pipeline: update_strain_{update_i}")
     with open(pipeline_yaml, "w") as f:
         yaml.dump(pipeline_config, f, sort_keys=False)
-
-    logger.info(f"Updated configs for {dataset_name}")
+    logger.info(f"Updated {pipeline_yaml}")
 
